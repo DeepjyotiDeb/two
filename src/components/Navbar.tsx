2b808d3e457986d529c441061f2a35f8935dc28e
DeepjyotiDeb <55659244+DeepjyotiDeb@users.noreply.github.com>
2022-11-05 17:26:48 +0530

    Navbar updates

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,68 +8,9 @@ import Sidebar from "./Sidebar";
 const Navbar: React.FC = () => {
   const { data: sessionData } = useSession();
   const [isOpen, setIsOpen] = useState(false);
-  //   return (
-  //     <header className="body-font  fixed top-0 z-50 w-full bg-gray-900 text-gray-400">
-  //       <div className="container absolute mx-auto flex h-20  flex-col flex-wrap p-5 md:static md:flex-row">
-  //         <a className="title-font mb-3 flex items-center font-medium text-white md:mb-0">
-  //           <svg
-  //             xmlns="http://www.w3.org/2000/svg"
-  //             fill="none"
-  //             stroke="currentColor"
-  //             strokeLinecap="round"
-  //             strokeLinejoin="round"
-  //             strokeWidth="2"
-  //             className="h-10 w-10 rounded-full bg-green-500 p-2 text-white"
-  //             viewBox="0 0 24 24"
-  //           >
-  //             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-  //           </svg>
-
-  //           <span className="ml-3 text-xl">SupaCode!</span>
-  //         </a>
-  //         <nav className="flex flex-col space-x-2 text-base md:ml-auto md:mr-auto md:flex-row md:items-center md:justify-center">
-  //           <Link
-  //             className="my-2 mr-5 cursor-pointer hover:text-green-500"
-  //             href="/posts"
-  //           >
-  //             Posts
-  //           </Link>
-  //           <Link
-  //             className="my-2 mr-5 cursor-pointer hover:text-green-500"
-  //             href="/posts"
-  //           >
-  //             Filler
-  //           </Link>
-  //         </nav>
-  //         <button
-  //           onClick={sessionData ? () => signOut() : () => signIn()}
-  //           className=" mt-4 inline-flex items-center rounded border-0 bg-gray-800 py-1 px-3 text-base hover:bg-gray-700 focus:outline-none md:mt-0"
-  //         >
-  //           {sessionData ? "Log Out" : "Log In"}
-  //           <svg
-  //             fill="none"
-  //             stroke="currentColor"
-  //             strokeLinecap="round"
-  //             strokeLinejoin="round"
-  //             strokeWidth="2"
-  //             className="ml-1 h-4 w-4"
-  //             viewBox="0 0 24 24"
-  //           >
-  //             <path d="M5 12h14M12 5l7 7-7 7"></path>
-  //           </svg>
-  //         </button>
-  //       </div>
-  //     </header>
-  //   );
-  // };
-
-  // export default Navbar;
-
-  // export default function Navbar() {
-  // const [navbarOpen, setNavbarOpen] = React.useState(false);
 
   return (
-    <div className="navbar fixed top-0 z-50 bg-gray-900">
+    <div className="navbar sticky top-0 z-50 bg-gray-900 ">
       <div className="navbar-start">
         <div className="dropdown">
           {/* <label tabIndex={0} className="btn-ghost btn lg:hidden">
@@ -115,7 +56,7 @@ const Navbar: React.FC = () => {
               <polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
             </svg>
           </label>
-          <ul
+          {/* <ul
             tabIndex={0}
             className="dropdown-content menu rounded-box menu-compact mt-3 w-52 bg-base-100 p-2 shadow"
           >
@@ -135,7 +76,7 @@ const Navbar: React.FC = () => {
                   <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
                 </svg>
               </a>
-              <ul className="p-2">
+              <ul className="bg-gray-900 p-2">
                 <li>
                   <a>Submenu 1</a>
                 </li>
@@ -147,7 +88,7 @@ const Navbar: React.FC = () => {
             <li>
               <a>Item 3</a>
             </li>
-          </ul>
+          </ul> */}
         </div>
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -183,7 +124,7 @@ const Navbar: React.FC = () => {
                 <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
               </svg>
             </a>
-            <ul className="p-2">
+            <ul className="bg-gray-900 p-2">
               <li>
                 <a>Submenu 1</a>
               </li>
@@ -197,13 +138,34 @@ const Navbar: React.FC = () => {
           </li>
         </ul>
       </div>
-      <div className="navbar-end">
+      <div className="navbar-end flex gap-4">
         <div className="form-control">
           <input
             type="text"
             placeholder="Search"
             className="input-bordered input input-sm w-full max-w-xs"
           />
+        </div>
+        <div className="dropdown-end dropdown">
+          <label
+            tabIndex={0}
+            className="placeholder btn-sm btn-circle avatar btn"
+          >
+            <div className="w-16 rounded-full bg-white text-neutral-content">
+              <span className="text-lg">J</span>
+            </div>
+          </label>
+          <ul
+            tabIndex={0}
+            className="dropdown-content menu rounded-box mt-4 w-52 bg-white text-black shadow"
+          >
+            <li className="hover:bg-gray-900 hover:text-white">
+              <a>Item 1</a>
+            </li>
+            <li className="hover:bg-gray-900 hover:text-white">
+              <a>Item 2</a>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
