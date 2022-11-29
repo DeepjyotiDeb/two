a99d9ac47cde6138d9ca8ba90a8423e30baa18b8
DeepjyotiDeb <55659244+DeepjyotiDeb@users.noreply.github.com>
2022-11-29 09:59:13 +0530

    vercel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
 import { NextPage } from "next";
 import Posts from "../components/posts";
-import { createProxySSGHelpers } from "@trpc/react-query/ssg";
-import { appRouter } from "../server/trpc/router/_app";
 
 const PostsPage: NextPage = () => {
   return <Posts />;
